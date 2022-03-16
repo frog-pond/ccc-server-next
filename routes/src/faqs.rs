7c863888ef26f096334afb0dfec2cbc6e11ffb60
Drew Volz <volz.drew@gmail.com>
2022-03-15 22:34:23 -0700

    annotate router function with must_use

--- routes/src/faqs.rs
+++ routes/src/faqs.rs
@@ -1,6 +1,7 @@
 use axum::{routing::get, Router};
 use handlers::github::faqs_handler;
 
+#[must_use]
 pub fn router() -> Router {
     Router::new().route("/", get(faqs_handler))
 }
--- routes/src/transit.rs
+++ routes/src/transit.rs
@@ -1,6 +1,7 @@
 use axum::{routing::get, Router};
 use handlers::github::{transit_bus_handler, transit_modes_handler};
 
+#[must_use]
 pub fn router() -> Router {
     Router::new()
         .route("/bus", get(transit_bus_handler))
