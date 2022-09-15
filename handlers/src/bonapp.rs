5c4af137498a0224bf74c0afe64cd2304e1bc22e
Kristofer Rye <kristofer.rye@gmail.com>
2022-09-15 16:45:25 -0500

    bonapp: Implement "named" cafe type (via enum?)
    I'm not sure I like this style honestly, hence it's private for now.
    
    We should probably just do something like a map from somewhere.  Config
    file? Database? Just hard-coded? We'll see.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- handlers/src/bonapp.rs
+++ handlers/src/bonapp.rs
@@ -1,3 +1,51 @@
+const BONAPP_CAFE_KEY_STAV: &str = "stav-hall";
+const BONAPP_CAFE_KEY_CAGE: &str = "the-cage";
+const BONAPP_CAFE_KEY_KINGS_ROOM: &str = "kings-room";
+const BONAPP_CAFE_KEY_BURTON: &str = "burton";
+const BONAPP_CAFE_KEY_LDC: &str = "ldc";
+const BONAPP_CAFE_KEY_SAYLES: &str = "sayles";
+const BONAPP_CAFE_KEY_WEITZ: &str = "weitz";
+
+#[derive(Debug)]
+enum NamedBonAppCafe {
+	Stav,
+	Cage,
+	KingsRoom,
+	Burton,
+	Ldc,
+	Sayles,
+	Weitz,
+}
+
+impl NamedBonAppCafe {
+	fn from_name(name: &str) -> Option<Self> {
+		use NamedBonAppCafe::*;
+		match name {
+			BONAPP_CAFE_KEY_STAV => Some(Stav),
+			BONAPP_CAFE_KEY_CAGE => Some(Cage),
+			BONAPP_CAFE_KEY_KINGS_ROOM => Some(KingsRoom),
+			BONAPP_CAFE_KEY_BURTON => Some(Burton),
+			BONAPP_CAFE_KEY_LDC => Some(Ldc),
+			BONAPP_CAFE_KEY_SAYLES => Some(Sayles),
+			BONAPP_CAFE_KEY_WEITZ => Some(Weitz),
+			_ => None,
+		}
+	}
+
+	const fn get_bonapp_cafe_id(&self) -> u32 {
+		use NamedBonAppCafe::*;
+		match self {
+			Stav => 261,
+			Cage => 262,
+			KingsRoom => 263,
+			Burton => 35,
+			Ldc => 36,
+			Sayles => 24,
+			Weitz => 458,
+		}
+	}
+}
+
 #[derive(serde::Serialize, serde::Deserialize)]
 #[serde(untagged)]
 enum ProxyRequestQueryParameters {
