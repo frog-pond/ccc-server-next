9023cf3fdfe92d6ea004b2f9ec79f3afa074f16c
Kristofer Rye <kristofer.rye@gmail.com>
2022-09-15 18:14:36 -0500

    types: Add BonAppCafesResponse (mostly autogenerated)
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- types/src/food.rs
+++ types/src/food.rs
@@ -29,8 +29,8 @@ pub struct ItemResponse {
 pub struct PauseMenuItemResponse {
 	station_menus: Vec<StationMenu>,
 	food_items: Vec<ItemResponse>,
-	#[ts(type = "Array<any>")]
-	cor_icons: Vec<Value>,
+	// #[ts(type = "Array<any>")]
+	cor_icons: std::collections::HashMap<String, BonAppCorIcon>,
 }
 
 #[derive(Serialize, Deserialize, TS)]
@@ -43,4 +43,69 @@ pub struct PauseMenuResponse {
 #[derive(Serialize, Deserialize, TS)]
 #[serde(rename_all = "snake_case")]
 #[ts(export)]
-pub struct BonAppCafesResponse {}
+pub struct BonAppCafesResponse {
+	// #[ts(type = "Map<String, BonAppCafeResponse>")]
+	cafes: std::collections::HashMap<String, BonAppCafeResponse>,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(rename_all = "snake_case")]
+#[ts(export)]
+pub struct BonAppCafeResponse {
+	name: String,
+	address: String,
+	city: String,
+	state: String,
+	zip: String,
+	latitude: String,
+	longitude: String,
+	description: String, // html
+	message: String,     // html
+	eod: String,         // 02:00
+	timezone: String,    // America/Chicago
+	menu_type: String,   // dynamic
+	menu_html: String,
+	location_detail: String,
+	weekly_schedule: String, // html
+	days: Vec<BonAppDay>,
+	cor_icons: std::collections::HashMap<String, BonAppCorIcon>,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[ts(export)]
+pub struct BonAppCorIcon {
+	#[ts(type = "any")]
+	sort: Option<serde_json::Value>,
+	label: String,
+	description: String,
+	image: String,
+	is_filter: IsFilter,
+	allergen: i64,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[ts(export)]
+pub struct BonAppDay {
+	date: String,
+	dayparts: Vec<BonAppDaypart>,
+	status: String,
+	message: String,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[ts(export)]
+pub struct BonAppDaypart {
+	id: String,
+	starttime: String,
+	endtime: String,
+	message: Option<String>,
+	label: String,
+	hide: String,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[ts(export)]
+pub enum IsFilter {
+	N,
+	Y,
+}
