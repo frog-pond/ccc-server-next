92cd9902d5d8165a4c43402f548841fbfbf9817c
Kristofer Rye <kristofer.rye@gmail.com>
2022-09-16 09:44:00 -0500

    routes: Add by-id menu and cafe routes using subhandlers
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- routes/src/food.rs
+++ routes/src/food.rs
@@ -1,12 +1,14 @@
 use axum::{routing::get, Router};
 use handlers::{
-	bonapp::{named_cafe_handler, named_cafe_menu_handler},
+	bonapp::{cafe, cafe_menu, named_cafe_handler, named_cafe_menu_handler},
 	github::pause_menu_handler,
 };
 
 #[must_use]
 pub fn router() -> Router {
 	Router::new()
+		.route("/menu/:cafe_id", get(cafe_menu))
+		.route("/cafe/:cafe_id", get(cafe))
 		.route("/named/cafe/:name", get(named_cafe_handler))
 		.route("/named/menu/:name", get(named_cafe_menu_handler))
 		.route("/named/menu/the-pause", get(pause_menu_handler))
