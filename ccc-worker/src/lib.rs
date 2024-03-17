dee2ca8282fcc51d725c9fb59889d44b0f13fecf
Kristofer Rye <kristofer.rye@gmail.com>
2024-03-17 13:23:42 -0500

    worker: Implement basic prototype
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- ccc-worker/src/lib.rs
+++ ccc-worker/src/lib.rs
@@ -1,1 +1,32 @@
+use core::{future::Future, pin::Pin};
+use std::time::Duration;
 
+use tokio_util::task::TaskTracker;
+
+enum WorkerTask {
+	Generic(Pin<Box<dyn Future<Output = ()> + Send>>),
+}
+
+struct WorkerTaskTracker {
+	tracker: TaskTracker,
+}
+
+impl WorkerTaskTracker {
+	fn new() -> Self {
+		let tracker = TaskTracker::new();
+		Self { tracker }
+	}
+
+	fn spawn(&self, task: WorkerTask) {
+		match task {
+			WorkerTask::Generic(future) => {
+				self.tracker.spawn(future);
+			}
+		}
+	}
+
+	async fn wait(&self) {
+		self.tracker.close();
+		self.tracker.wait().await
+	}
+}
