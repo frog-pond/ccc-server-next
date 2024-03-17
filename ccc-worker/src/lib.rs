3cf5bc9e8e76366c2ae839997dc1fe625f873a4a
Kristofer Rye <kristofer.rye@gmail.com>
2024-03-17 15:06:08 -0500

    worker: Resolve a couple of clippy warnings and add docs
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- ccc-worker/src/lib.rs
+++ ccc-worker/src/lib.rs
@@ -1,23 +1,69 @@
 use core::{future::Future, pin::Pin};
-use std::time::Duration;
 
 use tokio_util::task::TaskTracker;
 
-enum WorkerTask {
+/// A task that can be run by a ccc-specific worker.
+///
+/// These are specific types of tasks that are unique to the ccc-server;
+/// they are not meant to be used in other contexts.
+///
+/// # Variants
+///
+/// - [`WorkerTask::Generic`] - A generic future task that can be spawned.
+///   For now, this is the only variant.
+///
+/// # Examples
+///
+/// ```
+/// use ccc_worker::{Worker, WorkerTask};
+/// use tokio::time::Duration;
+///
+/// let task = WorkerTask::Generic(Box::pin(async {
+///     tokio::time::sleep(Duration::from_millis(100)).await;
+/// }));
+/// ```
+pub enum WorkerTask {
+	/// A wrapper around any old `async` block that returns ().
 	Generic(Pin<Box<dyn Future<Output = ()> + Send>>),
 }
 
-struct WorkerTaskTracker {
+/// A worker that can be used to run tasks in the background.
+///
+/// # Examples
+///
+/// ```
+/// use ccc_worker::{Worker, WorkerTask};
+/// use tokio::time::Duration;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let worker = Worker::new();
+///     worker.spawn(WorkerTask::Generic(Box::pin(async {
+///         tokio::time::sleep(Duration::from_millis(100)).await;
+///     })));
+///     worker.signal_done_spawning_tasks();
+///     worker.wait().await;
+/// }
+/// ```
+pub struct Worker {
 	tracker: TaskTracker,
 }
 
-impl WorkerTaskTracker {
-	fn new() -> Self {
+impl Default for Worker {
+	fn default() -> Self {
 		let tracker = TaskTracker::new();
-		Self { tracker }
+		Self::new()
+	}
+}
+
+impl Worker {
+	pub fn new() -> Self {
+		Self {
+			..Default::default()
+		}
 	}
 
-	fn spawn(&self, task: WorkerTask) {
+	pub fn spawn(&self, task: WorkerTask) {
 		match task {
 			WorkerTask::Generic(future) => {
 				self.tracker.spawn(future);
@@ -25,8 +71,11 @@ impl WorkerTaskTracker {
 		}
 	}
 
-	async fn wait(&self) {
+	pub fn signal_done_spawning_tasks(&self) {
 		self.tracker.close();
+	}
+
+	pub async fn wait(&self) {
 		self.tracker.wait().await
 	}
 }
