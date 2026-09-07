0e52bfcb43be6f382224fe52761c2b2cdd49e442
Kristofer Rye <kristofer.rye@gmail.com>
2026-09-07 14:11:32 -0500

    crate: Create crate for `trifecta`
    There are several parts of this crate that want to work very similarly,
    and the goal of this project applies broadly to all of its routes: to
    collect a response from a source, perhaps apply some sort of
    transformation to it, store it for some period of time, retrieve it (and
    to do this part especially quickly), and then serve it up to the client.
    
    Additionally, one should be able to program a route/module easily and in
    a contained way so that they *contribute* their behavior as much as
    possible, less-so than "handling" things.
    
    This project will not be an HTTP proxy, nor Axum middleware. HTTP is one
    possible transport, and Axum is one possible framework. Instead we will
    speak in terms of "operations", "sources", "transformations", and
    "routes".
    
    More to come.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ trifecta/src/lib.rs
@@ -0,0 +1,3 @@
+// Documentation TODO: introduce Trifecta's operation model with one complete
+// uncached example, then contrast transport integration, typed resolution assembly,
+// cache bypass, future cache/singleflight configuration, and runtime neutrality.
